2634b9efc339915d27f668a39fc070275f41b060
Adam Myczkowski <mycaxd511@gmail.com>
2017-07-20 20:57:23 +0100

    1.4: Add reset button that resets the input fields and the output field
    Signed-off-by: Adam Myczkowski <mycaxd511@gmail.com>

--- src/main/java/com/android/mycax/luckynumbers/MainActivity.java
+++ src/main/java/com/android/mycax/luckynumbers/MainActivity.java
@@ -8,7 +8,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        View.OnClickListener {
 
 
     // Used to load the 'native-lib' library on application startup.
@@ -18,8 +19,10 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText mFirstName;
     private EditText mLastName;
-    private Button mButton;
-    
+    private TextView mOutputText;
+    private Button mButton_Calculate;
+    private Button mButton_Reset;
+
 
     private boolean isEmpty(EditText etText) {
         return etText.getText().toString().trim().length() == 0;
@@ -34,26 +37,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFirstName = (EditText)findViewById(R.id.firstName);
-        mLastName = (EditText)findViewById(R.id.lastName);
-        mButton = (Button)findViewById(R.id.button);
+        mFirstName = (EditText) findViewById(R.id.firstName);
+        mLastName = (EditText) findViewById(R.id.lastName);
+        mOutputText= (TextView) findViewById(R.id.output_text);
 
-        mButton.setOnClickListener(
-                new View.OnClickListener()
-                {
-                    public void onClick(View view) {
-                        if (isEmpty(mFirstName) && isEmpty(mLastName) || isEmpty(mFirstName) || isEmpty(mLastName)) {
-                            Toast.makeText(MainActivity.this, "Input fields cannot be empty", Toast.LENGTH_SHORT).show();
+        mButton_Calculate = (Button) findViewById(R.id.button_calculate);
+        mButton_Calculate.setOnClickListener(this);
+        mButton_Reset = (Button) findViewById(R.id.button_reset);
+        mButton_Reset.setOnClickListener(this);
+    }
 
-                        } else if (isAlpha(mFirstName.getText().toString()) && isAlpha(mLastName.getText().toString())) {
-                            TextView tv = (TextView) findViewById(R.id.output_text);
-                            tv.setText(Calculate(mFirstName.getText().toString(), mLastName.getText().toString()));
-                        }
-                        else {
-                            Toast.makeText(MainActivity.this, "Input fields can only contain letters!", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button_calculate) {
+            if (isEmpty(mFirstName) && isEmpty(mLastName) || isEmpty(mFirstName) || isEmpty(mLastName)) {
+                Toast.makeText(MainActivity.this, "Input fields cannot be empty", Toast.LENGTH_SHORT).show();
+
+            } else if (isAlpha(mFirstName.getText().toString()) && isAlpha(mLastName.getText().toString())) {
+                mOutputText.setText(Calculate(mFirstName.getText().toString(), mLastName.getText().toString()));
+            } else {
+                Toast.makeText(MainActivity.this, "Input fields can only contain letters!", Toast.LENGTH_SHORT).show();
+            }
+        } else if (v.getId() == R.id.button_reset) {
+            mFirstName.getText().clear();
+            mLastName.getText().clear();
+            mOutputText.setText("");
+        }
     }
 
     public native String Calculate(String jFirstName, String jLastName);
