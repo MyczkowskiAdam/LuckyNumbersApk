6a3cf3150c212def682df3765e0ecb96881fc787
Adam Myczkowski <mycaxd511@gmail.com>
2017-07-20 15:26:23 +0100

    LuckyNumberAPK: Optimise the algorithm
    Sync with 06fdc0f77c870d80afc97fc8b0dacecc9205f333
    
    Signed-off-by: Adam Myczkowski <mycaxd511@gmail.com>

--- src/main/cpp/luckynumbers.cpp
+++ src/main/cpp/luckynumbers.cpp
@@ -22,10 +22,9 @@ const std::string lucky[] = {
 const std::string alphabet = "abcdefghijklmnopqrstuvwxyz"; //Letters of alphabet used throughout the program
 const std::string CAPalphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //Capital letters of alphabet used throughout the program
 
-int FrNmVal, LstNmVal, tmp, LuckyNumber, dig1, dig2, dig3;
+int NmVal, FrNmVal, LstNmVal, tmp, LuckyNumber, dig1, dig2, dig3;
 
 std::string FrNmFromJava, LstNmFromJava, NumberAndMeaning;
-char *FrNmAr, *LstNmAr;
 
 /* END VARIABLES LIST*/
 
@@ -37,77 +36,55 @@ std::string ToString(T pNumber)
     return oOStrStream.str();
 }
 
+int convert(std::string Nm) {
+    char *NmAr = const_cast<char*>(Nm.c_str()); // converts a string into char* (pointer to a char, basically a char array)
+
+    for (unsigned int i = 0; i<Nm.length(); i++) {
+        tmp = alphabet.find(NmAr[i]); // This function takes a char from char*, tries to find it in the alphabet string, returns its positiion as a integer and writes results to tmp integer
+        if (tmp == std::string::npos) { // npos (-1) is returned when char we looking for is not in a given string (no position)
+            tmp = CAPalphabet.find(NmAr[i]); // This function takes a char from char*, tries to find it in the CAPalphabet string, returns its positiion as a integer and writes results to tmp integer
+            if (tmp == std::string::npos) { // npos (-1) is returned when char we looking for is not in a given string (no position)
+                break;
+            }
+        }
+        NmVal += (tmp % 9) + 1; // Use simple mathematical equation to change the tmp int into a right number as in the cheatsheet, for example 'c' is in pos 2, 2 % 9 = 2 because 2 / 9 = 0.(2), 2 + 1 = 3. ^Lookup cheatsheet: c = 3.
+    }
+    int cNmVal = NmVal;
+    NmVal = 0;
+    return cNmVal;
+}
+
+int splitNumber(int Number) {
+    while (Number > 9) { // if number is lower than 9 it's a single digit and doesn't need to be split
+        dig1 = (Number / 10) % 10;
+        dig2 = Number % 10;
+        if (Number > 99) {
+            dig3 = (Number / 100) % 10;
+            Number = dig1 + dig2 + dig3;
+        }
+        else {
+            Number = dig1 + dig2;
+        }
+    }
+    return Number;
+}
 int LuckyNumbersProcess(std::string FrNm, std::string LstNm) {
-    FrNmAr = const_cast<char*>(FrNm.c_str()); // converts a string into char* (pointer to a char, basically a char array)
+    FrNmVal = convert(FrNm);
+    FrNmVal = splitNumber(FrNmVal);
 
-    for (unsigned int i = 0; i<FrNm.length(); i++) {
-        tmp = int(alphabet.find(FrNmAr[i])); // This function takes a char from char*, tries to find it in the alphabet string, returns its positiion as a integer and writes results to tmp integer
-        if (tmp == std::string::npos) { // npos (-1) is returned when char we looking for is not in a given string (no position)
-            tmp = int(CAPalphabet.find(FrNmAr[i])); // This function takes a char from char*, tries to find it in the CAPalphabet string, returns its positiion as a integer and writes results to tmp integer
-            if (tmp == std::string::npos) { // npos (-1) is returned when char we looking for is not in a given string (no position)
-                break;
-            }
-        }
-        FrNmVal += (tmp % 9) + 1; // Use simple mathematical equation to change the tmp int into a right number as in the cheatsheet, for example 'c' is in pos 2, 2 % 9 = 2 because 2 / 9 = 0.(2), 2 + 1 = 3. ^Lookup cheatsheet: c = 3.
-    }
-
-    LstNmAr = const_cast<char*>(LstNm.c_str()); // converts a string into char* (pointer to a char, basically a char array)
-
-    for (unsigned int i = 0; i<LstNm.length(); i++) {
-        tmp = int(alphabet.find(LstNmAr[i])); // This function takes a char from char*, tries to find it in the alphabet string, returns its positiion as a integer and writes results to tmp integer
-        if (tmp == std::string::npos) { // npos (-1) is returned when char we looking for is not in a given string (no position)
-            tmp = int(CAPalphabet.find(LstNmAr[i])); // This function takes a char from char*, tries to find it in the CAPalphabet string, returns its positiion as a integer and writes results to tmp integer
-            if (tmp == std::string::npos) { // npos (-1) is returned when char we looking for is not in a given string (no position)
-                break;
-            }
-        }
-        LstNmVal += (tmp % 9) + 1; // Use simple mathematical equation to change the tmp int into a right number as in the cheatsheet, for example 'c' is in pos 2, 2 % 9 = 2 because 2 / 9 = 0.(2), 2 + 1 = 3. ^Lookup cheatsheet: c = 3.
-    }
-
-    while (FrNmVal > 9) { // if number is lower than 9 it's a single digit and doesn't need to be split
-        dig1 = (FrNmVal / 10) % 10;
-        dig2 = FrNmVal % 10;
-        if (FrNmVal > 99) {
-            dig3 = (FrNmVal / 100) % 10;
-            FrNmVal = dig1 + dig2 + dig3;
-        }
-        else {
-            FrNmVal = dig1 + dig2;
-        }
-    }
-
-    while (LstNmVal > 9) { // if number is lower than 9 it's a single digit and doesn't need to be split
-        dig1 = (LstNmVal / 10) % 10;
-        dig2 = LstNmVal % 10;
-        if (LstNmVal > 99) {
-            dig3 = (LstNmVal / 100) % 10;
-            LstNmVal = dig1 + dig2 + dig3;
-        }
-        else {
-            LstNmVal = dig1 + dig2;
-        }
-    }
+    LstNmVal = convert(LstNm);
+    LstNmVal = splitNumber(LstNmVal);
 
     LuckyNumber = FrNmVal + LstNmVal;
 
-    while (LuckyNumber > 9) { // if number is lower than 9 it's a single digit and doesn't need to be split
-        dig1 = (LuckyNumber / 10) % 10;
-        dig2 = LuckyNumber % 10;
-        if (LuckyNumber > 99) {
-            dig3 = (LuckyNumber / 100) % 10;
-            LuckyNumber = dig1 + dig2 + dig3;
-        }
-        else {
-            LuckyNumber = dig1 + dig2;
-        }
-    }
+    LuckyNumber = splitNumber(LuckyNumber);
 
     NumberAndMeaning = "Your lucky number is " + ToString(LuckyNumber) + "!! This means that you " + lucky[LuckyNumber - 1];
     return 0;
 }
 
 void ClearAll() {
-    FrNmVal = LstNmVal = tmp = LuckyNumber = 0;
+    FrNmVal = LstNmVal = tmp = LuckyNumber = NmVal = 0;
     FrNmFromJava.clear();
     LstNmFromJava.clear();
     NumberAndMeaning.clear();
