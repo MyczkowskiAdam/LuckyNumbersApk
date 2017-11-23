70abbdf6da2c46cee47d5ac977a78a3844213aab
Adam Myczkowski <mycaxd511@gmail.com>
2017-11-23 01:03:24 +0000

    Add simple sqlite database to store the results
    Signed-off-by: Adam Myczkowski <mycaxd511@gmail.com>

---
+++ app/src/main/java/com/luckynumbers/mycax/luckynumbers/DataB.java
@@ -0,0 +1,71 @@
+package com.luckynumbers.mycax.luckynumbers;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+public class DataB extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 2;
+    public static final String DATABASE_NAME = "results_database";
+    public static final String RESULTS_TABLE_NAME = "results_table";
+    public static final String RESULTS_COLUMN_ID = "_id";
+    public static final String RESULTS_COLUMN_NAME = "name";
+    public static final String RESULTS_COLUMN_RESULT = "result";
+
+    public DataB(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE " + RESULTS_TABLE_NAME + " (" +
+                RESULTS_COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                RESULTS_COLUMN_NAME + " TEXT, " +
+                RESULTS_COLUMN_RESULT + " TEXT" + ")");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + RESULTS_TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+
+    public void saveToDB(String fname, String lname, String result) {
+        SQLiteDatabase database = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        String name = fname + " " + lname;
+        values.put(DataB.RESULTS_COLUMN_NAME, name);
+        values.put(DataB.RESULTS_COLUMN_RESULT, result);
+        database.insert(DataB.RESULTS_TABLE_NAME, null, values);
+        database.close();
+    }
+
+    public void ReadDB(Context context, ListView listView) {
+        SQLiteDatabase database = getReadableDatabase();
+
+        String[] from = new String[]{DataB.RESULTS_COLUMN_NAME, DataB.RESULTS_COLUMN_RESULT};
+        int[] to = new int[]{R.id.list_name, R.id.list_result};
+
+        String[] projection = {
+                DataB.RESULTS_COLUMN_ID,
+                DataB.RESULTS_COLUMN_NAME,
+                DataB.RESULTS_COLUMN_RESULT
+        };
+
+        Cursor cursor = database.query(DataB.RESULTS_TABLE_NAME, projection, null, null, null, null, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(context, R.layout.row, cursor, from, to, 0);
+        adapter.notifyDataSetChanged();
+
+        listView.setAdapter(adapter);
+        database.close();
+
+    }
+}
--- app/src/main/java/com/luckynumbers/mycax/luckynumbers/ResultsFragment.java
+++ app/src/main/java/com/luckynumbers/mycax/luckynumbers/ResultsFragment.java
@@ -5,14 +5,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class ResultsFragment extends Fragment {
+
+    private ListView listView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_results,
                 container, false);
+        listView = (ListView) view.findViewById(R.id.contentlist);
+        DataB database = new DataB(getActivity());
+        database.ReadDB(getActivity(), listView);
         return view;
     }
+
 }
